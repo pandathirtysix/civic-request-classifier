c93abcfb5b78a908433a75f9877fb43adf5ab637
PANDAG36 <ripwitepubg@gmail.com>
2026-08-19 01:30:28 +0530

    all models are created sucessfully and working fine

---
+++ pipeline/model_prediction.py
@@ -0,0 +1,63 @@
+import pickle
+from utils.ohe import ListOfData
+import numpy as np
+
+
+class ModelsPredictions:
+    def __init__(self):
+        #models
+        self.model_intent = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\model_intent.pkl", "rb"))
+        self.model_department = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\model_department.pkl", "rb"))
+        self.model_severity = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\model_severity.pkl", "rb"))
+        self.model_urgency = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\model_urgency.pkl", "rb"))
+        self.model_category = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\model_category.pkl", "rb"))
+
+        #label encoders
+        self.cat_LE = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\cat_LE.pkl", "rb"))
+        self.dep_LE = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\dep_LE.pkl", "rb"))
+        self.ug_LE = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\ug_LE.pkl", "rb"))
+        self.severity_LE = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\severity_LE.pkl", "rb"))
+        self.intent_LE = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\ohe_intent.pkl", "rb"))
+
+        #tfidf
+        self.tfidf_eg = pickle.load(open(r"X:\PROGRAMS\NLP PROJECT\MODELS\tfidf_english_gloss.pkl", "rb"))
+
+
+
+    def model_intent_prediction(self, text):
+        text = self.tfidf_eg.transform([text.lower()])
+        text = text.toarray()
+        prediction = self.model_intent.predict(text)
+        result_intent = self.intent_LE.inverse_transform(prediction)
+        return result_intent
+
+    def model_department_prediction(self, text):
+            text = self.tfidf_eg.transform([text.lower()])
+            text = text.toarray()
+            prediction = self.model_department.predict(text)
+            result_intent = self.dep_LE.inverse_transform(prediction)
+            return result_intent
+
+    def model_severity_prediction(self, text):
+            text = self.tfidf_eg.transform([text.lower()])
+            text = text.toarray()
+            prediction = self.model_severity.predict(text)
+            result_intent = self.severity_LE.inverse_transform(prediction)
+            return result_intent
+
+    def model_urgency_prediction(self, text):
+            text = self.tfidf_eg.transform([text.lower()])
+            text = text.toarray()
+            prediction = self.model_urgency.predict(text)
+            result_intent = self.ug_LE.inverse_transform(prediction)
+            return result_intent
+
+    def model_category_prediction(self, text):
+            text = self.tfidf_eg.transform([text.lower()])
+            text = text.toarray()
+            prediction = self.model_category.predict(text)
+            result_intent = self.cat_LE.inverse_transform(prediction)
+            return result_intent
+    
+
+        
--- utils/ohe.py
+++ utils/ohe.py
@@ -1,8 +1,49 @@
 from sklearn.preprocessing import OneHotEncoder
 
-class OHE:
+class ListOfData:
     def __init__(self):
         pass
 
     def ohe(self, col):
         pass
+
+    def intent_list(self):
+        self.intent_col = ['application_help', 'complaint', 'document_help', 'escalation',
+       'follow_up', 'information_request', 'status_query']
+
+        return self.intent_col
+
+    def category_list(self):
+        self.category_col =['certificates', 'drainage', 'electricity', 'public_health',
+               'roads', 'sanitation', 'street_lighting', 'transport',
+               'water_supply', 'welfare']
+        
+        return self.category_col
+
+
+
+    def deparment_list(self):
+        self.deparment_col = ['Drainage Department', 'Electricity Department',
+       'Public Health Department', 'Revenue Department',
+       'Roads Department', 'Sanitation Department',
+       'Transport Department', 'Water Supply Department',
+       'Welfare Department']
+        
+        return self.deparment_col
+
+    def urgency_list(self):
+        self.urgency_col = ['high', 'low', 'medium']
+        
+        return self.urgency_col
+
+    def severity_list(self):
+        self.severity = ['authority_issue', 'health_hazard', 'health_issue', 'health_risk',
+       'information', 'information_query', 'public_hazard',
+       'service_delay', 'service_failure', 'service_issue',
+       'service_request', 'status']
+
+        return self.severity
+
+    
+
+    
